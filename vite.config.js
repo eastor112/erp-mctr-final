25c99e8d121bb7d59fb65e1d60d88fb606aff96b
Emerson Asto <emerar.mct@gmail.com>
2021-07-24 23:21:51 -0500

    reparado bug

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
-// https://vitejs.dev/config/
-
 export default defineConfig({
   base: '/erp-mctr-final/',
   // base: '/',
